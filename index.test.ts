0fd8bfa22a6c034a39151aed6a80e88fe38f9ce7
Rob Funnell <robfunnell@Robs-MacBook-Air-2.local>
2020-05-02 20:13:49 +0100

    added randomised date generator to test

--- index.test.ts
+++ index.test.ts
@@ -1,12 +1,28 @@
 import {expect} from "chai";
 import {bookHoliday} from "./index"
 
+
 describe('Test', () => {
+    let minDate;
+    let maxDate;
+
+    beforeEach(() => {
+        const randomDate = (start, end) => {
+            return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+        };
+        minDate = new Date();
+        maxDate = randomDate(minDate, new Date(2020, 4, 31));
+    });
 
     it ('should confirm booking details', () => {
-        expect(bookHoliday(6, [2020, 10, 9], [2020, 10, 11])).to.eql(`Group size: 6
-Check in / out: Mon Nov 09 2020 - Wed Nov 11 2020`);
+        const myVacation = bookHoliday(6, minDate, maxDate);
+        console.log(myVacation);
+        expect(myVacation).to.eql(`Group size: 6
+Check in / out: ${minDate.toDateString()} - ${maxDate.toDateString()}`);
+
     });
+
+
 
 
 });
--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
-export const bookHoliday = (numOfGuests: number, checkIn: [number, number, number], checkOut: [number, number, number]) => {
-    const checkInDate = new Date(...checkIn);
-    const checkOutDate = new Date(...checkOut);
+export const bookHoliday = (numOfGuests: number, checkIn, checkOut) => {
+    const checkInDate = new Date(checkIn);
+    const checkOutDate = new Date(checkOut);
+    const ONE_DAY = 1000 * 60 * 60 * 24;
+    const lengthOfStay = (checkOutDate.getTime() - checkInDate.getTime()) / ONE_DAY;
 
     return `Group size: ${numOfGuests}
 Check in / out: ${checkInDate.toDateString()} - ${checkOutDate.toDateString()}`
